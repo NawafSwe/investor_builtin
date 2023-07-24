f1b949abecb1b71f2ca67e0a2c676148218bbcf9
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-24 20:21:17 +0300

    feat: creat alert rules table in sql init

--- dev_setup/sql/init.sql
+++ dev_setup/sql/init.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE IF NOT EXISTS investor_bulletin;
+
+CREATE TABLE IF NOT EXISTS alert_rules (
+  id INTEGER NOT NULL,
+  symbol TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL UNIQUE,
+  threshold_price REAL NOT NULL,
+  PRIMARY KEY (id)
+);
