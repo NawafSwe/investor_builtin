239a373bd0b9c1eda0e0633b2cc15a82466c4fe7
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-24 21:04:09 +0300

    feat: setup db connection on application startup

--- api/main.py
+++ api/main.py
@@ -2,8 +2,10 @@ from uvicorn import run
 from fastapi import FastAPI
 
 from api.routes import init_routes
+from db.models.model_base import setup_db
 
 app = init_routes(FastAPI())
 
 if __name__ == "__main__":
+    setup_db()
     run("api.main:app")
--- db/models/model_base.py
+++ db/models/model_base.py
@@ -1,3 +1,26 @@
-# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from api.config import Settings
 
-# Base = declarative_base()
+settings = Settings()
+
+
+SQLALCHEMY_DATABASE_URI = f'cockroachdb://root@{settings.DB_HOST}:{settings.DB_PORT}/investor_bulletin?sslmode=disable'
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def setup_db():
+    Base.metadata.create_all(bind=engine)
