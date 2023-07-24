45b27d88d37f89bc69de722a938301b6c7f8df14
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-24 18:33:20 +0300

    feat: return market prices

--- resources/market/market_service.py
+++ resources/market/market_service.py
@@ -3,8 +3,28 @@
 this file to write any business logic for the Market
 """
 
-import requests
+from requests import get
+from constants.symbols import Symbol
+from api.config import Settings
+
+settings = Settings()
+
+
+def _construct_symbols_str():
+    symbols = ""
+    for s in Symbol:
+        symbols += s.value + ","
+    return symbols
 
 
 def get_market_data():
-    return requests.get()
+    api_key = settings.TWELVE_DATA_API_KEY
+
+    params = {"symbol": _construct_symbols_str(), "dp": 2}
+    response = get("https://api.twelvedata.com/price",
+                   params=params,
+                   headers={"Content-Type": "application/json", "Authorization": f"apikey {api_key}"})
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.content, "status": response.status_code}
