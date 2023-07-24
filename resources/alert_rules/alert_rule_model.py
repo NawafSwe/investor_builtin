df69e34a50148e2a3fc6836bdd738acade03aa3e
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-24 21:04:37 +0300

    feat: create alert rule model

--- resources/alert_rules/alert_rule_model.py
+++ resources/alert_rules/alert_rule_model.py
@@ -1,5 +1,12 @@
 """ Alert Rule Model """
-# from db.models.model_base import Base
+from sqlalchemy import Column, Integer, String, Float
 
-# class AlertRule(Base):
-#     __tablename__ = "alert-rules"
+from db.models.model_base import Base
+
+
+class AlertRule(Base):
+    __tablename__ = "alerts_rules"
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    symbol = Column(String, unique=True, index=True, nullable=False)
+    threshold_price = Column(Float(10, 2))
