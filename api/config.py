177a0693d5d61d584973a66b054bbc64a00c1912
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-24 21:02:38 +0300

    feat: add db config

--- api/config.py
+++ api/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     TWELVE_DATA_API_KEY: str
+    DB_HOST: str
+    DB_PORT: int
 
     class Config:
         env_file = ".env"
