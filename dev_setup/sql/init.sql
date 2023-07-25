97d255d8d6a1f3d7237f21cb9dde424d2cbaa2b3
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-25 18:34:02 +0300

    feat: change alert rule constraint

--- dev_setup/sql/init.sql
+++ dev_setup/sql/init.sql
@@ -2,7 +2,8 @@ CREATE DATABASE IF NOT EXISTS investor_bulletin;
 USE investor_bulletin;
 CREATE TABLE IF NOT EXISTS alerts_rules (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  symbol TEXT NOT NULL UNIQUE,
-  name TEXT NOT NULL UNIQUE,
-  threshold_price REAL NOT NULL
+  symbol TEXT NOT NULL,
+  name TEXT NOT NULL,
+  threshold_price REAL NOT NULL,
+  CONSTRAINT _alert_rule_constraint UNIQUE (symbol, threshold_price)
 );
