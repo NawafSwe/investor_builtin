9c9ebcd5a3dd7fd627e14c674a8c9218f6744899
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-25 17:22:07 +0300

    feat: implement remaining CRUD, ops for alert rules

--- api/controllers/rules_controller.py
+++ api/controllers/rules_controller.py
@@ -20,3 +20,22 @@ def create_alert_rule(body: AlertRuleCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(alert_rule)
     return {"message": "Alert Rule created successfully", "data": alert_rule, "status": True, "status_code": 201}
+
+
+@router.get("/{alert_rule_id}")
+def get_alert_rule_by_id(alert_rule_id: str, db: Session = Depends(get_db)):
+    alert_rule = db.query(AlertRule).filter(AlertRule.id == alert_rule_id).first()
+    if alert_rule is None:
+        return {"message": "Alert Rule not found", "status": False, "status_code": 404}
+
+    return {"data": alert_rule, "status": True, "status_code": 200}
+
+
+@router.delete("/{alert_rule_id}", status_code=204)
+def delete_alert_rule_by_id(alert_rule_id: str, db: Session = Depends(get_db)):
+    alert_rule = db.query(AlertRule).filter(AlertRule.id == alert_rule_id).first()
+    if alert_rule is None:
+        return
+
+    db.delete(alert_rule)
+    db.commit()
