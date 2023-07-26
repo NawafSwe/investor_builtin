27dbdb47f017338ee2e8f392cc881384e813eccf
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-26 14:48:36 +0300

    feat: update schema

--- dev_setup/sql/init.sql
+++ dev_setup/sql/init.sql
@@ -1,9 +1,21 @@
 CREATE DATABASE IF NOT EXISTS investor_bulletin;
 USE investor_bulletin;
-CREATE TABLE IF NOT EXISTS alerts_rules (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  symbol TEXT NOT NULL,
-  name TEXT NOT NULL,
-  threshold_price REAL NOT NULL,
-  CONSTRAINT _alert_rule_constraint UNIQUE (symbol, threshold_price)
+CREATE TABLE IF NOT EXISTS alerts_rules
+(
+    id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    symbol          TEXT NOT NULL,
+    name            TEXT NOT NULL,
+    threshold_price REAL NOT NULL,
+    created_at      DATE NOT NULL    DEFAULT now(),
+    updated_at      DATE NOT NULL    DEFAULT now(),
+    CONSTRAINT _alert_rule_constraint UNIQUE (symbol, threshold_price)
+);
+
+CREATE TABLE alerts
+(
+    id         UUID            DEFAULT gen_random_uuid() PRIMARY KEY,
+    name       STRING NOT NULL,
+    symbol     STRING NOT NULL,
+    created_at DATE   NOT NULL DEFAULT now(),
+    updated_at DATE   NOT NULL DEFAULT now()
 );
