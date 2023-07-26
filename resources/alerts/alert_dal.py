f4bd12374841e4586444cd0ccb1997060902ba0b
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-26 15:34:57 +0300

    feat: implement alert repo

--- resources/alerts/alert_dal.py
+++ resources/alerts/alert_dal.py
@@ -1,10 +1,36 @@
 """ Alert DAL"""
+from abc import ABC
+from typing import List, Type
+
+from sqlalchemy.orm import Session
+
+from mixins.repository import Repository
+from resources.alerts.alert_schema import CreateAlertCommand
+from db.models import Alert
+
 """_summary_
 this file is to right any ORM logic for the Alert model
 """
-# from resources.alerts.alert_schema import AlertCreate
-# from db.models import Rule
 
-# def create_rule( rule: AlertCreate, session ):
-#     new_alert = Alert()
-#     session.add(new_alert)
+
+class AlertRepository(Repository, ABC):
+    """
+       AlertRuleRepository
+       used as abstraction to access data layer and return objects
+       requires: sqlalchemy.orm.Session
+       """
+
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create(self, command: CreateAlertCommand):
+        alert = Alert(
+            name=command.name,
+            symbol=command.symbol,
+            original_threshold_price=command.original_threshold_price,
+            reached_threshold_price=command.reached_threshold_price)
+        self.db.add(alert)
+        return alert
+
+    def find_all(self) -> List[Type[Alert]]:
+        return self.db.query(Alert).all()
