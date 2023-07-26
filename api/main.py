15dd41f27ee8cf6215ec08bdcfe7e5c9e7d40391
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-26 13:36:28 +0300

    feat: receive message success from RabbitMQ

--- api/main.py
+++ api/main.py
@@ -5,21 +5,36 @@ from fastapi import FastAPI
 
 from api.routes import init_routes
 from db.models.model_base import setup_db
-from event_subscriber.main import setup_pika_connection
+from event_subscriber.main import EventHandler
+from api.config import Settings
 
+settings = Settings()
 app = init_routes(FastAPI())
-
+event_handler = EventHandler()
 if __name__ == "__main__":
     setup_db()
     run("api.main:app")
 
 
-@app.get("/health_check")
-def health():
+@app.get("/health-check")
+async def health():
     return {"status": "ok"}
 
 
+async def _startup_event():
+    await event_handler.connect()
+    await event_handler.subscribe(
+        exchange_key="alert_threshold",
+        router_key="alert_threshold_price_reached",
+        queue=settings.ALERTS_QUEUE,
+        exchange_type="topic",
+    )
+
+    await event_handler.publish(exchange_key="alert_threshold",
+                                router_key="alert_threshold_price_reached", exchange_type="topic", message="hello here event")
+
+
 @app.on_event("startup")
-async def on_startup_event():
+async def startup():
     loop = asyncio.get_running_loop()
-    loop.create_task(setup_pika_connection())
+    loop.create_task(_startup_event())
--- event_subscriber/main.py
+++ event_subscriber/main.py
@@ -1,8 +1,12 @@
+import asyncio
 import json
+import uuid
+from functools import partial
+from typing import Callable
 
-import aiormq
-from fastapi.logger import logger
-
+import aio_pika
+from aiormq.abc import DeliveredMessage
+from pika import BasicProperties
 
 from api.config import Settings
 
@@ -10,22 +14,61 @@ from api.config import Settings
 settings = Settings()
 
 
-async def setup_pika_connection():
-    # create connection
-    mq_url = f"amqp://{settings.BROKER_USERNAME}:{settings.BROKER_USERNAME}@{settings.BROKER_HOST}"
-    connection = await aiormq.connect(url=mq_url)
-    # create channel
-    channel = await connection.channel()
-    # create queue
-    await channel.queue_declare(queue=settings.ALERTS_QUEUE)
+class EventHandler:
 
-    async def callback(channel, body, envelope, properties):
-        from resources.alerts.alert_service import create_alert
-        correlation_id = properties.correlation_id
-        logger.log(f"Received message with correlation ID {correlation_id}: {body}")
-        await channel.basic_client_ack(delivery_tag=envelope.delivery_tag)
-        if body:
-            create_alert(json.loads(body))
+    def __init__(self):
+        self._mq_url = f"amqp://{settings.BROKER_USERNAME}:{settings.BROKER_USERNAME}@{settings.BROKER_HOST}"
+        self._exchange_key = "alert_threshold"
+        self._router_key = "alert_threshold_price_reached"
+        self.connection = None
+        self.channel = None
 
-    # Start consuming messages from the queue
-    await channel.basic_consume(queue=settings.ALERTS_QUEUE, consumer_callback=callback)
+    async def connect(self):
+        self.connection = await aio_pika.connect_robust(url=self._mq_url)
+        self.channel = await self.connection.channel()
+
+    async def subscribe(
+            self,
+            exchange_key: str = None,
+            router_key: str = None,
+            exchange_type: str = None,
+            queue: str = None,
+            durable: bool = False,
+    ):
+        await self.channel.set_qos(prefetch_count=1)
+        exchange = await self.channel.declare_exchange(
+            name=exchange_key,
+            type=exchange_type,
+        )
+        queue = await self.channel.declare_queue(name=queue, durable=durable)
+        await queue.bind(exchange, routing_key=router_key)
+        await queue.consume(self.message_callback)
+
+    async def close_connection(self):
+        await self.connection.close()
+
+    @staticmethod
+    async def message_callback(message: aio_pika.IncomingMessage):
+        async with message.process():
+            body = message.body.decode()
+            correlation_id = message.headers.get("correlation_id")
+            print("message received here, id: ", correlation_id)
+            print("message received here, body: ", body)
+            # await callback(body, correlation_id)
+
+    async def publish(
+            self,
+            exchange_key: str = None,
+            router_key: str = None,
+            exchange_type: str = None,
+            message: str = None,
+    ):
+        exchange = await self.channel.declare_exchange(
+            name=exchange_key,
+            type=exchange_type,
+        )
+        props = {"correlation_id": str(uuid.uuid4())}
+        await exchange.publish(
+            aio_pika.Message(body=message.encode(), headers=props),
+            routing_key=router_key,
+        )
