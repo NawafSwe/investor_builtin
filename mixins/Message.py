eb9035645b5e36b1afefd3a9939a2eb1641c449f
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-26 14:58:48 +0300

    feat: update message mixin

--- mixins/Message.py
+++ mixins/Message.py
@@ -1,22 +1,34 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, asdict
+
+from typing import TypedDict
 
 
 class Message(ABC):
     """
     Message interface to be used as abstraction to encapsulate data and send it to the broker or consume it as command
     """
-    pass
+
+    @abstractmethod
+    def dict(self):
+        raise NotImplementedError("dict method is not implemented")
 
 
+@dataclass
 class Command(Message):
     """
     Command interface to be used as abstraction to encapsulate data and send it to the broker or consume it as command
     """
-    pass
+
+    def dict(self):
+        return {k: str(v) for k, v in asdict(self).items()}
 
 
+@dataclass
 class Event(Message):
     """
     Event interface to be used as abstraction to encapsulate data and send it to the broker or consume it as event
     """
-    pass
+
+    def dict(self):
+        return {k: str(v) for k, v in asdict(self).items()}
