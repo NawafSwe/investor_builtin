31156e815be19188f853012c0cc45ecc336f5c4e
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-26 14:59:50 +0300

    feat: add alert model

--- resources/alerts/alert_model.py
+++ resources/alerts/alert_model.py
@@ -1,5 +1,16 @@
 """ Alert Model """
-# from db.models.model_base import Base
+import uuid
+from datetime import datetime
 
-# class Alert(Base):
-#     __tablename__ = "alerts"
+from sqlalchemy import Column, String, UUID, Date
+
+from db.models.model_base import Base
+
+
+class Alert(Base):
+    __tablename__ = "alerts"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    name = Column(String, index=True, nullable=False)
+    symbol = Column(String, index=True, nullable=False)
+    created_at = Column(Date, index=True, nullable=False, default=datetime.now())
+    updated_at = Column(Date, index=True, nullable=False, default=datetime.now())
