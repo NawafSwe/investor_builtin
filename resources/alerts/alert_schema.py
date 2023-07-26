612e00a4d99358191ca0f1bfebe17cac66fa7d98
NawafSwe <nawaf.jr9@hotmail.com>
2023-07-26 15:35:21 +0300

    feat: implement alert command

--- resources/alerts/alert_schema.py
+++ resources/alerts/alert_schema.py
@@ -1,7 +1,15 @@
 """ Alert Schema """
+
+from mixins.Message import Command
+from pydantic import BaseModel
+
 """_summary_
 This file to abstract any validation logic for the Alerts
 """
-# from pydantic import BaseModel
 
-# class AlertCreate(BaseModel):
+
+class CreateAlertCommand(BaseModel, Command):
+    symbol: str
+    name: str
+    original_threshold_price: str
+    reached_threshold_price: str
